1ebe0d8c48ca6c529e77b7b8a25b4cfe36e49f89
rasha knifdi <rasha.knifdi@chasacademy.se>
2026-02-18 00:05:40 +0100

    perf: remove unnecessary script code and defer non-critical JS to improve initial render

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-
+// delete  simulateHeavyWork()  blocking code
 
 document.addEventListener("DOMContentLoaded", function () {
-    const items = document.querySelectorAll(".add-to-cart");
-    items.forEach(function (item) {
-        item.addEventListener("click", function () {
+    document.querySelectorAll(".add-to-cart").forEach(btn => {
+        btn.addEventListener("click", () => {
             alert("Item added to cart!");
         });
     });
 
+
+
     const form = document.querySelector(".newsletter form");
+    //
     if (form) { form.addEventListener("submit", function (e)
     { e.preventDefault();
         alert("Thank you for subscribing!");
